31b237bc050c0249eb8a0fe0173aa2f87ffa1e30
Atuuh <not-another-one@hotmail.com>
2022-05-08 19:01:04 +0100

    Bootstrapped twitch auto shoutouts

---
+++ apps/twitch/autoshoutout/jest.config.ts
@@ -0,0 +1,13 @@
+module.exports = {
+  displayName: 'twitch-autoshoutout',
+  preset: '../../../jest.preset.ts',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': [
+      '@swc/jest',
+      { jsc: { transform: { react: { runtime: 'automatic' } } } },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/apps/twitch/autoshoutout',
+};
