bf3989af97e410a709e0ec406f7afacfc23ed751
Atuuh <not-another-one@hotmail.com>
2022-05-11 20:09:33 +0100

    Moved more stuff

--- apps/synacor/src/main.ts
+++ apps/synacor/src/main.ts
@@ -1,1 +1,9 @@
+import { reader, getVM } from '@projects/synacor-lib';
+
 console.log('Hello World!');
+
+const data = reader('./libs/synacor/challenge.bin');
+
+const vm = getVM({ logger: (val) => process.stdout.write(val) });
+
+vm.run(data);
--- libs/synacor-lib/src/index.ts
+++ libs/synacor-lib/src/index.ts
@@ -1,10 +1,4 @@
-import { reader } from './lib/reader';
-import { getVM } from './lib/synacor';
+export { reader } from './lib/reader';
+export { getVM } from './lib/synacor';
 
 export * from './lib/synacor';
-
-const data = reader('./libs/synacor/challenge.bin');
-
-const vm = getVM({ logger: (val) => process.stdout.write(val) });
-
-vm.run(data);
